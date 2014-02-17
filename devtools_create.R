8672b852a06585f56d839ab34dbbef1a601cac44
Ben Moore <b3nmoore@gmail.com>
2014-02-17 15:52:55 +0000

    put into package framework, add figure 4 script

---
+++ devtools_create.R
@@ -0,0 +1,8 @@
+setwd("~/hvl/production/3dgenome/")
+
+library("devtools")
+install_github("devtools")
+
+options(devtools.desc.author="'Benjamin L. Moore <b3nmoore@gmail.com> [aut,cre]'")
+options(devtools.desc.license="MIT")
+load_all()
