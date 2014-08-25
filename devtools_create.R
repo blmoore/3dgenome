12fa171528276c3883ed976793133a88b411f4e4
Benjamin L. Moore <blmoore@users.noreply.github.com>
2014-08-25 10:55:34 +0100

    Delete devtools_create.R

--- devtools_create.R
+++
@@ -1,8 +0,0 @@
-setwd("~/hvl/production/3dgenome/")
-
-library("devtools")
-install_github("devtools")
-
-options(devtools.desc.author="'Benjamin L. Moore <b3nmoore@gmail.com> [aut,cre]'")
-options(devtools.desc.license="MIT")
-load_all()
