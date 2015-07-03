f42e60d53dd6d100ab1f21a666087b6d0c98e319
Ben Moore <b3nmoore@gmail.com>
2015-07-03 16:01:28 +0100

    rpts

--- R/t_repeatClasses.R
+++ R/t_repeatClasses.R
@@ -1,7 +1,12 @@
 ### Test repeat classes over boundaries
 # RepeatMasked tracks from UCSC table browser 30 June
-library("blmR")
+library("dplyr")
+library("ggplot2")
 library("readr")
+library("reshape2")
+library("blmR")
+
+setwd("~/hvl/repeats/")
 
 hrep <- read_tsv("~/hvl/repeats/hg19_ucscRepeatMasker.txt")
 mrep <- read_tsv("~/hvl/repeats/mm10_ucscRepeatMasker.txt")
@@ -60,11 +65,11 @@ buildBoundariesDat <- function(ct=c("h1", "k5", "gm"), type=c("c", "t"),
     # compartments
     pattern <- paste0("^", ct, "cb_.*?", col, ".*\\.bed\\.out")
     #steps <- 30
-    steps <- 25
+    steps <- 19
   } else {
     # TADs
     pattern <- paste0("^", ct, "tb_.*?", col, ".*\\.bed\\.out")
-    steps <- 25
+    steps <- 19
   }
   
   fl <- list.files("~/hvl/repeats/", pattern=pattern, full.names = T)
@@ -119,50 +124,52 @@ buildBoundariesDat <- function(ct=c("h1", "k5", "gm"), type=c("c", "t"),
   }
 }
 
-hc <- buildBoundariesDat("h1", type = "c", col="repFamily")
-gc <- buildBoundariesDat("gm", type = "c", col="repFamily")
-kc <- buildBoundariesDat("k5", type = "c", col="repFamily")
+blob <- function(each){
+  each %>% group_by(ct, type, feat) %>% 
+    summarise(bound.mean = mean(X10),
+      edge.mean  = mean(c(X1, X2, X3, X17, X18, X19)),
+      p = wilcox.test(X10, c(X1, X2, X3,  X17, X18, X19))$p.value)
+}
 
-ht <- buildBoundariesDat("h1", type = "t", col="repFamily")
-kt <- buildBoundariesDat("k5", type = "t", col="repFamily")
-gt <- buildBoundariesDat("gm", type = "t", col="repFamily")
+fam_sum <- Map(buildBoundariesDat, c("h1", "gm", "k5"), rep(c("c", "t"), 3), col="repFamily", summary=T)
+fam_sum <- do.call(rbind, fam_sum)
+fam_each <- Map(buildBoundariesDat, c("h1", "gm", "k5"), rep(c("c", "t"), 3), col="repFamily", summary=F)
+fam_each <- do.call(rbind, fam_each)
 
+class_sum <- Map(buildBoundariesDat, c("h1", "gm", "k5"), rep(c("c", "t"), 3), col="repClass", summary=T)
+class_sum <- do.call(rbind, class_sum)
+class_each <- Map(buildBoundariesDat, c("h1", "gm", "k5"), rep(c("c", "t"), 3), col="repClass", summary=F)
+class_each <- do.call(rbind, class_each)
 
-pdf <- rbind(hc, gc, kc, ht, kt, gt)
-
-library("ggplot2")
-pdf("~/hvl/repeats/repFamily.pdf", 6, 14)
-ggplot(pdf, aes(x=pos, y=mean, col=type, fill=type)) + 
+## 1) profiles:
+pdf("plots/repFamily_v2.pdf", 6, 14)
+ggplot(fam_sum, aes(x=pos, y=mean, col=type, fill=type)) + 
   facet_grid(feat~ct, scales="free") + 
-  geom_vline(xintercept=12.5, lwd=4, col=I(rgb(.8,.8,.8, .5))) +
+  geom_vline(xintercept=10, lwd=4, col=I(rgb(.8,.8,.8, .5))) +
   geom_ribbon(aes(ymin=means.l, ymax=means.u), alpha=I(.3), col=I(NULL)) +
   geom_line() + theme_bw() +
-  scale_color_brewer(type="qual", palette = 2) + 
+  scale_color_brewer(type = "qual", palette = 2) + 
   scale_fill_brewer(type = "qual", palette = 2) +
-  scale_x_continuous(breaks=c(0, 12.5, 25), labels = c("-450 kb", "boundary", "+450 kb")) +
+  scale_x_continuous(breaks=c(0, 10, 19), labels = c("-450 kb", "boundary", "+450 kb")) +
+  labs(x="", y="Mean annotated repeats per bin", col="", fill="")
+dev.off()
+
+pdf("plots/repClass_v2.pdf", 6, 14)
+ggplot(class_sum, aes(x=pos, y=mean, col=type, fill=type)) + 
+  facet_grid(feat~ct, scales="free") + 
+  geom_vline(xintercept=10, lwd=4, col=I(rgb(.8,.8,.8, .5))) +
+  geom_ribbon(aes(ymin=means.l, ymax=means.u), alpha=I(.3), col=I(NULL)) +
+  geom_line() + theme_bw() +
+  scale_color_brewer(type = "qual", palette = 2) + 
+  scale_fill_brewer(type = "qual", palette = 2) +
+  scale_x_continuous(breaks=c(0, 10, 19), labels = c("-450 kb", "boundary", "+450 kb")) +
   labs(x="", y="Mean annotated repeats per bin", col="", fill="")
 dev.off()
 
 
-hcf <- buildBoundariesDat("h1", type = "c", col="repClass", F)
-gcf <- buildBoundariesDat("gm", type = "c", col="repClass", F)
-kcf <- buildBoundariesDat("k5", type = "c", col="repClass", F)
-
-htf <- buildBoundariesDat("h1", type = "t", col="repClass", F)
-ktf <- buildBoundariesDat("k5", type = "t", col="repClass", F)
-gtf <- buildBoundariesDat("gm", type = "t", col="repClass", F)
-
-pdff <- rbind(hcf, gcf, kcf, htf, ktf, gtf)
-
-library("dplyr")
-blobs <- group_by(pdff, ct, type, feat) %>% 
-  summarise(bound.mean = mean(X12),
-    edge.mean  = mean(c(X1, X2, X3, X4, X5, X25, X24, X23, X22, X21)),
-    p = wilcox.test(X12, c(X1, X2, X3, X4, X5, X25, X24, X23, X22, X21))$p.value)
-head(blobs)
-
-pdf("~/hvl/repeats/rptclass_bubble.pdf", 9, 5)
-ggplot(blobs, aes(x=feat, y=-log10(p), col=type,
+## 2) bubble significance plots
+pdf("plots/repClass_bubble_v2.pdf", 9, 5)
+ggplot(blob(class_each), aes(x=feat, y=-log10(p), col=type,
   size=abs(bound.mean - edge.mean))) +
   facet_grid(ct~., scales="free_y") + 
   geom_point(position=position_dodge(.15)) + theme_bw() +
@@ -176,3 +183,48 @@ ggplot(blobs, aes(x=feat, y=-log10(p), col=type,
   scale_colour_brewer(type="qual", palette=6) +
   ggtitle("Repeat classes over boundaries")
 dev.off()
+
+pdf("plots/repFamily_bubble_v2.pdf", 9, 5)
+ggplot(blob(fam_each), aes(x=feat, y=-log10(p), col=type,
+  size=abs(bound.mean - edge.mean))) +
+  facet_grid(ct~., scales="free_y") + 
+  geom_point(position=position_dodge(.15)) + theme_bw() +
+  labs(list(size="Absolute difference\nat boundary",
+    y=expression(-log[10](italic(p))),
+    x="", col="Boundary type")) + 
+  theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1)) +
+  scale_size_continuous(range=c(2,7)) +
+  guides(col=guide_legend(override.aes=list(size=4))) +
+  geom_hline(yintercept=-log10(.05 / 264), linetype="dashed") +
+  scale_colour_brewer(type="qual", palette=6) +
+  ggtitle("Repeat families over boundaries")
+dev.off()
+
+
+## ---- bp per bin ---- ##
+files <- list.files("~/hvl/repeats/", pattern = ".*?\\.reptable", full.names = T)
+
+bins <- read_tsv("~/hvl/repeats/h1_tb.bed", col_names = F)
+head(bins)
+nrow(bins)
+# source of bins: 
+s <- read_tsv("~/hvl/production/3dgenome/data/bedfiles/h1_tadbounds.bed")
+nrow(s)
+
+
+
+for( f in files ){
+  # debugging
+  f = files[[104]]
+  
+  f <- read_tsv(f, col_names = c("bin", "rep", "bp"))
+  
+  head(f)
+  library("reshape2")
+  rep <- dcast(bin ~ rep, value.var = "bp", data=f, fill=0)
+  
+  
+  
+  
+  
+  
