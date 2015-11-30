eb06057fd255de47c1d9f1f1ed05e22a6d160b67
Ben Moore <b3nmoore@gmail.com>
2015-11-30 21:01:19 +0000

    thesis correction change

--- R/t_chrpositioning.R
+++ R/t_chrpositioning.R
@@ -20,7 +20,9 @@ kdat <- readRDS("data/rds/K562_35Vars.rds")
 gdat <- readRDS("data/rds/Gm12878_35Vars.rds")
 
 chrs <- gsub("chr(.*?)-.*", "\\1", rownames(hdat))
-locales <- data.frame(pos=rep(pos[match(chrs, pos$chr),1], 3),
+locales <- data.frame(
+  chr=chrs,
+  pos=rep(pos[match(chrs, pos$chr),1], 3),
   ct=c(
       rep("GM12878", length(chrs)), 
       rep("H1 hESC", length(chrs)), 
@@ -29,7 +31,7 @@ locales <- data.frame(pos=rep(pos[match(chrs, pos$chr),1], 3),
   eig=c(gdat$eigen, hdat$eigen, kdat$eigen))
   
 
-locales$pos <- factor(locales$pos, levels=c("Edge", "no_bias", "Central"))
+locales$pos <- factor(locales$pos, levels=c("Edge", "No bias", "Central"))
 
 pdf("~/hvl/thesis_plots/nucpos.pdf", 6, 2)
 ggplot(subset(locales, pos %in% c("Edge", "Central")),
@@ -56,6 +58,42 @@ locales$comp <- as.factor(c(
 ggplot(subset(locales, pos %in% c("edge", "central")), aes(x=comp, fill=pos)) + 
   geom_histogram(position = position_fill()) + facet_wrap(~ct)
 
+library("dplyr")
+## Thesis corrections :: treat chromosomes as grouped
+per_chr <- locales[complete.cases(locales),] %>% group_by(ct, pos, chr) %>% summarise(mid=median(eig))
+
+levels(per_chr$chr) <- c(1:22, "X")
+
+pos = position_jitter()
+pdf("~/hvl/thesis_plots/nuc_pos_v2.pdf", 7, 3)
+ggplot(per_chr, aes(x=pos, y=mid, col=chr, group=pos)) +
+  geom_boxplot(outlier.size = NA, width=.3,
+    col=I("grey75"), alpha=I(.75), fill=I("grey75")) + 
+  #geom_point(position=pos) +
+  geom_text(aes(label=paste0("chr", chr)), 
+    position=position_jitter(), size=I(2.4),
+    adj=.1) +
+  facet_wrap(~ct) +
+  theme_minimal() +
+  theme(legend.position="none") +
+  labs(y="Median eigenvector per chromosome",
+    x="Nuclear positioning")
+dev.off()
+
+# h1
+wilcox.test(subset(per_chr, ct == "H1 hESC" & pos == "Edge")$mid,
+  subset(per_chr, ct == "H1 hESC" & pos == "Central")$mid)
+
+# gm12878
+wilcox.test(subset(per_chr, ct == "GM12878" & pos == "Edge")$mid,
+  subset(per_chr, ct == "GM12878" & pos == "Central")$mid)
+
+# k562
+wilcox.test(subset(per_chr, ct == "K562" & pos == "Edge")$mid,
+  subset(per_chr, ct == "K562" & pos == "Central")$mid)
+
+
+## Previous method :: continuous eigenvector comparison
 # h1
 ks.test(
   subset(locales, ct == "H1 hESC" & pos == "Edge")$eig,
